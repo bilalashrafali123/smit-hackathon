f5d9f9ff8c9a67b71617a7cac7e6642d34b9bddd
bilalashrafali123 <ashrafalibilal385@gmail.com>
2025-01-25 15:46:17 +0500

    login,logout,register page created
    login,logout,register page created

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import Logout from './pages/Logout.jsx'
+import Layout from './Layout.jsx'
+
+const router = createBrowserRouter([
+  {
+      path: "",
+      element: <Layout/>,
+      children: [
+          {
+              path: "",
+              element: <Home/>
+          },
+          {
+              path: "register",
+              element: <Register/>
+          },
+          {
+              path: "login",
+              element: <Login/>
+          },
+          {
+              path: "logout",
+              element: <Logout/>
+          },
+          
+      ]
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router}>
+  </RouterProvider>
 )
