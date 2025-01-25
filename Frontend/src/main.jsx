3856cfe9e83f5c1e8da432a858d7a45e554fe354
bilalashrafali123 <ashrafalibilal385@gmail.com>
2025-01-25 17:12:01 +0500

    done
    done

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,11 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import Login from './pages/Login.jsx'
-import Register from './pages/Register.jsx'
-import Logout from './pages/Logout.jsx'
-import Layout from './Layout.jsx'
+
 import App from './App.jsx'
+import Register from './Pages/Register.jsx'
+import Login from './Pages/Login.jsx'
+import Logout from './Pages/Logout.jsx'
+import Layout from './Layout.jsx'
 
 const router = createBrowserRouter([
   {
